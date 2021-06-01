b339efba39393a9734ee6fcedaf441bb7b5017ef
you06 <you1474600@gmail.com>
2021-06-02 00:42:50 +0800

    add favicon
    Signed-off-by: you06 <you1474600@gmail.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     title: 'Terminology Dict',
-    description: 'Terminology Dict collects term words.'
+    description: 'Terminology Dict collects term words.',
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "128x128", href: "/favicon.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "128x128", href: "/favicon.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "128x128", href: "/favicon.png"}],
+    ],
 }
