07f0560474d9f881f09e98cf22aecbcc5ff4f38f
you06 <you1474600@gmail.com>
2021-06-02 00:37:42 +0800

    load dict in page
    Signed-off-by: you06 <you1474600@gmail.com>

--- docs/.vuepress/components/dict.vue
+++ docs/.vuepress/components/dict.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
-        123
+        {{dict}}
     </div>
 </template>
 <script>
+const axios = require('axios')
+
 export default {
     data() {
         return {
-            dynamicComponent: null
+            dynamicComponent: null,
+            dict: 1
         }
     },
     mounted () {
-        
+        axios({
+            method: 'get',
+            url: '/dict'
+        })
+        .then(res => {
+            this.dict = res.data
+        })
+        .catch(err => {
+            // TODO: display error
+            console.log('axios err', err)
+        })
     }
 }
 </script>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    title: 'Terminology Dict',
+    description: 'Terminology Dict collects term words.'
+}
